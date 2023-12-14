d848a40d5ea151616daae6ca442d8d2b9fd16012
VoidsAreThere <t19.thakur@outlook.com>
2023-12-14 18:01:10 +0530

    Hind in tailwindcss

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -1,14 +1,40 @@
 export default function ContactUs() {
     return (
-        <div className="font-montserrat font-bold">
+        <>
             <div className="flex flex-col items-center justify-center">
-                <div className="text-3xl border-b-2 border-black border-dotted py-6 my-2">
+                <div className="text-3xl font-montserrat font-bold border-b-2 border-black border-dotted py-6 my-2">
                     Contact Us
                 </div>
-                <div>
+                <div className="font-hind text-[#666666] text-base text-center px-4 my-6">
                     Please call or email us to confirm your booking. We do welcome walk-ins for day picnic packages as well.
                 </div>
             </div>
-        </div>
+            <div className="flex flex-col justify-center px-4">
+                    <div>
+                        <div className="font-montserrat font-bold text-xl">
+                            Address
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-montserrat font-bold text-xl">
+                            Phone
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-montserrat font-bold text-xl">
+                            Customer Service Time
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+        </>
     );
 }
