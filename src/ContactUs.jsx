9ba668a45a0eeb4024c1a85adf2a0c2d76d1dc0f
VoidsAreThere <t19.thakur@outlook.com>
2023-12-14 17:41:10 +0530

    Added Hind

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -1,8 +1,13 @@
 export default function ContactUs() {
     return (
-        <div className="font-montserrat">
-            <div>
-                Contact Us
+        <div className="font-montserrat font-bold">
+            <div className="flex flex-col items-center justify-center">
+                <div className="text-3xl border-b-2 border-black border-dotted py-6 my-2">
+                    Contact Us
+                </div>
+                <div>
+                    Please call or email us to confirm your booking. We do welcome walk-ins for day picnic packages as well.
+                </div>
             </div>
         </div>
     );
