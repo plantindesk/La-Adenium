e29dcb85687d6afb3a0d8f8999045ce4d031d6b9
Raj Santosh Mhatre <5021131@fcrit.onmicrosoft.com>
2023-12-14 18:29:11 +0530

    AboutUS Changes

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,3 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import 'tailwindcss/tailwind.css';
+
+export default function AboutUs() {
+    return (
+        <div className="relative">
+        {/* Image */}
+        <img
+          src="https://picsum.photos/300/200"
+          alt="Banner Image"
+          className="w-full h-auto"
+        />
+  
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+  
+        {/* Text Container */}
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+          <h2 className="text-4xl font-bold mb-4">Your Banner Text</h2>
+          <p className="text-lg">Subtitle or additional information</p>
+        </div>
+      </div>
+    );
+    
+}
 
